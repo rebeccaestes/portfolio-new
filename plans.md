5fd537b9d7465aed5b667ec4d1eb85ca7600484f
Rebecca Estes <estes.rebecca@gmail.com>
2016-03-11 14:09:08 -0500

    consider parallelogram & more/less fxnality, sassify

--- plans.md
+++ plans.md
@@ -1,1 +1,9 @@
-Animate header
+- Projects: port from old site, switch side of images, thinner borders
+
+- Animate header
+
+- Shade alternating categories
+
+- Header as paralellogram
+
+- Clean up & SASSify CSS
--- script.js
+++ script.js
@@ -6,4 +6,34 @@ $(document).ready(function(){
 	// 	rso_skills.addClass("skills-used");
 	// })
 
+	$(".more").click(function(e) {
+		$(e.target).closest(".project_about").find(".more_about").fadeIn("slow");
+		// $(e.target).fadeOut("slow");
+		// $(e.target).css("color", "white");
+		$(e.target).hide();
+		$(e.target).next().show();
+	})
+
+	$(".less").click(function(e) {
+		$(e.target).closest(".project_about").find(".more").fadeIn("slow");
+		$(e.target).closest(".project_about").find(".more_about").fadeOut("slow");
+		$(e.target).hide();
+		$(e.target).previous().show()
+
+	})
+
+	$(".kitten-link").click(function() {
+		$(".container").each(function() {
+			$(this).css("opacity", ".5");
+		});
+		$(".kitten").fadeIn("slow");
+	})
+
+	$(".kitten-close").click(function() {
+		$(".kitten").fadeOut("slow");
+		$(".container").each(function() {
+			$(this).fadeTo("slow", 1)
+		}) 
+	})
+
 });
