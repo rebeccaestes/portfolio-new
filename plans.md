bbb4e46347a5bde2e73da48332829012a85a2fa6
Rebecca Estes <estes.rebecca@gmail.com>
2016-03-10 23:12:24 -0500

    initial commit

---
+++ plans.md
@@ -0,0 +1,1 @@
+Animate header
---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+	// $(".readysetom").on("click", function() {
+	// 	var rso_skills = $(".skills").find($(".rso"));
+	// 	console.log(rso_skills);
+	// 	rso_skills.addClass("skills-used");
+	// })
+
+});
